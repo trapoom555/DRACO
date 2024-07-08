31a7fea0e2203acc98596a1cc48f9ef6575bbd18
trapoom555 <trapoom555@gmail.com>
2024-07-08 20:05:55 +0800

    can load dataset

--- data/data.py
+++ data/data.py
@@ -12,11 +12,9 @@ class CalvinDataset(Dataset):
     calvin debug dataset, may be changed to WDS for the full dataset
     """
 
-    def __init__(self, image_fn, text_fn, dataset_path, is_train=True) -> None:
+    def __init__(self, dataset_path, is_train=True) -> None:
         super().__init__()
         self.dataset_path = dataset_path
-        self.image_fn = image_fn
-        self.text_fn = text_fn
 
         tag = "training" if is_train else "validation"
         self.file_prefix = f"{self.dataset_path}/{tag}"
@@ -37,27 +35,10 @@ class CalvinDataset(Dataset):
             f"{self.file_prefix}/episode_{frame:07d}.npz"
         )  # , allow_pickle=True (lazy load)
         rgb_static = Image.fromarray(frame["rgb_static"])
+        depth_static = Image.fromarray(frame["depth_static"])
         rgb_gripper = Image.fromarray(frame["rgb_gripper"])
+        depth_gripper = Image.fromarray(frame["depth_gripper"])
         actions = np.array(frame["rel_actions"])
         
         actions[..., 6:] = (actions[..., 6:] + 1) // 2
-        return rgb_static, text, actions
-
-    def collater(self, sample):
-        images = [s[0] for s in sample]
-        texts = [s[1] for s in sample]
-        actions = [s[2] for s in sample]
-
-        image_tensors = self.image_fn(images)
-        text_tensors = self.text_fn(texts)
-        action_tensors = torch.FloatTensor(np.stack(actions))
-        return image_tensors, text_tensors, action_tensors
-
-
-def load_pkl(filename: Path) -> Dict[str, np.ndarray]:
-    with open(filename, "rb") as f:
-        return pickle.load(f)
-
-
-def load_npz(filename: Path) -> Dict[str, np.ndarray]:
-    return np.load(filename.as_posix())
+        return rgb_static, depth_static, rgb_gripper, depth_gripper, text, actions
